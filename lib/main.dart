c8473a9fd046256aa7b9466f37c6109fde7e1c4f
kishaMM <kishammejia@gmail.com>
2024-11-02 02:22:45 -0600

    3rd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,288 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: ProfileScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ProfileScreen extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  Widget build(BuildContext context) 
+  {
+    return Scaffold(
+      
+      appBar: AppBar(
+        leading: null,
+      ),
+      
+      body: 
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          
+          CircleAvatar(
+            radius: 40,
+            backgroundColor: Color(0xFFC4EEC0),
+            child: Image.asset('assets/images/capPan.jpeg'),
+            //backgroundImage: AssetImage('assets/images/capPan.jpg'),
+          ),
+          
+          SizedBox(height: 20),
+          
+          Text(
+            'Coffeestories',
+            style: 
+            TextStyle(
+              fontSize: 30, 
+              fontWeight: FontWeight.w600,
+              ),
+          ),
+          
+          Text(
+            'mark.brock@icloud.com',
+            style: TextStyle(
+              fontSize: 15, 
+              color: Colors.grey[800],
+              fontWeight: FontWeight.w400,
+              ),
+          ),
+          
+          SizedBox(height: 20),
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+          ElevatedButton(
+            onPressed: () {},
+            child: 
+            Text(
+              'Edit profile',
+              style: TextStyle(
+                fontSize: 18, 
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+              ),
+            ),
+            style: 
+            ElevatedButton.styleFrom(
+              backgroundColor: Colors.black,
+              shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          
+          SizedBox(height: 40),
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+          heading("Inventories"),
+          
+          SizedBox(height: 10),
+
+          inventoriesCard(),
+
+          SizedBox(height: 10),
+
+          heading("Preferences"),
+
+          SizedBox(height: 10),
+
+          preferencesCard(),
+        ],
+      ),
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+// widgetsssss
+
+  Widget heading(String heading){
+    return Container(
+      padding: EdgeInsets.only(left: 16.0),
+      alignment: Alignment.centerLeft,
+      child: Text(
+        heading,
+        style: TextStyle(
+        color: Colors.grey[700],
+        fontSize: 15,
+        fontWeight: FontWeight.w400,
+        ),
+      )
+    );
+  }
+
+  Widget inventoriesCard(){
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Card(
+        color: Colors.grey[200],
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            ListTile(
+              
+              leading: Container(
+                width: 40,
+                height:40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Icon(Icons.storefront),
+              ),
+              
+              title: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('My Stores'),
+                  SizedBox(width: 8),
+                  Container(
+                    padding: EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      color: Colors.green[700],
+                      shape: BoxShape.circle,
+                    ),
+                  constraints: BoxConstraints(
+                    minWidth: 30,
+                    minHeight: 30,
+                  ),
+                  child: Center(
+                    child: Text(
+                      '2',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              trailing: Icon(Icons.arrow_forward, color: Colors.grey[600])
             ),
+            Divider(
+              color: Colors.grey[600],
+              height: 1,
+              indent: 10,
+              endIndent: 10,
+            ),
+            ListTile(
+              leading: Container(
+                width: 40,
+                height:40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Icon(Icons.support),
+              ),
+              title: Text('Support'),
+              trailing: Icon(Icons.arrow_forward, color: Colors.grey[600]),
+            )
+          ]
+        ),
+      ),
+    );
+  }
+  
+  Widget preferencesCard(){
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Card(
+        color: Colors.grey[200],
+        child: Column(
+          children: [
+            
+            //1
+            ListTile(
+              leading: Container(
+                width: 40,
+                height:40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Icon(Icons.notifications),
+              ),
+              title: Text('Push notifications'),
+              trailing: Switch(activeTrackColor: Colors.green[700], value: true, onChanged: (value) {}),
+            ),
+            
+            Divider(
+              color: Colors.grey[600],
+              height: 1,
+              indent: 10,
+              endIndent: 10,
+            ),
+            //2
+            ListTile(
+              leading: Container(
+                width: 40,
+                height:40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Icon(Icons.mood_rounded),
+              ),
+              title: Text('Face ID'),
+              trailing: Switch(activeTrackColor: Colors.green[700], value: true, onChanged: (value) {}),
+            ),
+            
+            Divider(
+              color: Colors.grey[600],
+              height: 1,
+              indent: 10,
+              endIndent: 10,
+            ),
+            
+            //3
+            ListTile(
+              leading: Container(
+                width: 40,
+                height:40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Icon(Icons.fiber_pin_rounded),
+              ),
+              title: Text('PIN Code'),
+              trailing: Icon(Icons.arrow_forward, color: Colors.grey[600])
+            ),
+            
+            Divider(
+              color: Colors.grey[600],
+              height: 1,
+              indent: 10,
+              endIndent: 10,
+            ),
+            
+            //4
+            ListTile(
+              leading: Container(
+                width: 40,
+                height:40,
+                decoration: BoxDecoration(
+                  color: Colors.red[50],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child:  Icon(Icons.logout_outlined, color: Colors.red[900],),
+              ),
+              title: Text(
+                'Logout',
+                style: TextStyle(color: Colors.red[900]),
+                ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+
 }
